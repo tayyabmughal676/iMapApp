b1c266250e2de13df12d18b599685cefa4dfb77c
tayyabmughal676 <tayyabmughal676@gmail.com>
2021-12-22 11:03:09 +0500

    Initial Commit

---
+++ iMapApp/iMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMapAppApp.swift
+//  iMapApp
+//
+//  Created by Thor on 22/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct iMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
